19ba277554ccfc69008c990fab8e9ba9def8178e
Gheorghe Tarcea <tarcea@gmail.com>
2020-03-12 12:58:28 +0100

    validation on models - done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,5 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-  validates :description, uniqueness: true, presence: true
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :ingredient_id, uniqueness: { scope: :cocktail_id }
+  validates :description, presence: true
 end
